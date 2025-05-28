503f536736ad4f9042dee386597be82222e1591d
Ankit Singh <133861762+singhankit6748@users.noreply.github.com>
2025-05-28 07:09:37 +0530

    Update solution.java

--- 3sum/solution.java
+++ 3sum/solution.java
@@ -1,53 +1,40 @@
-class Solution {
-    public List<List<Integer>> fourSum(int[] nums, int target) {
+import java.util.*;
 
-        List<List<Integer>> result = new ArrayList<>();
-
+public class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-
-        int n = nums.length;
-
-        for(int i = 0 ; i  < n - 3 ; i++)
-        {
-            if( i > 0 && nums[i] == nums[i -1]) continue;
-
-            for(int j = i + 1 ; j < n -  2 ; j++)
-            {
-                if(j > i + 1 && nums[j] == nums[j -1]) continue;
+        List<List<Integer>> result = new ArrayList<>();
+        
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
             
-
-            int left = j + 1;
-            int right = n - 1;
-
-            while(left < right)
-            {
-                long sum = (long)nums[i] + nums[j] + nums[left] + nums[right];
-
-                if(sum == target)
-                {
-                    result.add(Arrays.asList(nums[i],nums[j],nums[left],nums[right]));
-
+            int left = i + 1, right = nums.length - 1;
+            
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    
                     left++;
                     right--;
-
-                    while(left < right && nums[left] == nums[left - 1]) left++;
-                    while(left < right && nums[right] == nums[right + 1]) right--;
-                }
-                else if(sum < target)
-                {
+                } else if (sum < 0) {
                     left++;
-                }
-                else
-                {
+                } else {
                     right--;
                 }
-
-            }
-
             }
         }
-
-        return result;
         
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        System.out.println(new Solution().threeSum(nums));
     }
 }
+
