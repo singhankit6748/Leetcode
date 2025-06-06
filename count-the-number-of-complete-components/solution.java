8cb4274646d49d9e57f66e94bb0dbcfc901e46f7
Ankit Singh <133861762+singhankit6748@users.noreply.github.com>
2025-06-06 09:46:46 +0530

    Update solution.java

--- count-the-number-of-complete-components/solution.java
+++ count-the-number-of-complete-components/solution.java
@@ -1,1 +1,52 @@
- 
+ import java.util.*;
+
+public class Solution {
+    public int countCompleteComponents(int n, int[][] edges) {
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int[] edge : edges) {
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]);
+        }
+
+        boolean[] visited = new boolean[n];
+        int completeCount = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                List<Integer> component = new ArrayList<>();
+                dfs(i, graph, visited, component);
+                
+                boolean isComplete = checkComplete(component, graph);
+                if (isComplete) completeCount++;
+            }
+        }
+
+        return completeCount;
+    }
+
+    private void dfs(int node, List<List<Integer>> graph, boolean[] visited, List<Integer> component) {
+        visited[node] = true;
+        component.add(node);
+        for (int neighbor : graph.get(node)) {
+            if (!visited[neighbor]) {
+                dfs(neighbor, graph, visited, component);
+            }
+        }
+    }
+
+    private boolean checkComplete(List<Integer> component, List<List<Integer>> graph) {
+        int size = component.size();
+        for (int node : component) {
+            if (graph.get(node).size() != size - 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
