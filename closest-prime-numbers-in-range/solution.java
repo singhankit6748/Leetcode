9407b2cca752bfb3d60c4b93e2382c5a03605a1c
Ankit Singh <133861762+singhankit6748@users.noreply.github.com>
2025-06-06 09:37:47 +0530

    Update solution.java

--- closest-prime-numbers-in-range/solution.java
+++ closest-prime-numbers-in-range/solution.java
@@ -1,1 +1,54 @@
- 
+ import java.util.*;
+
+public class Solution { 
+    public int[] closestPrimes(int left, int right) {
+        
+        int limit = (int) 1e6;
+        boolean[] isPrime = new boolean[limit + 1];
+        Arrays.fill(isPrime, true);
+        isPrime[0] = isPrime[1] = false; 
+
+        for (int i = 2; i * i <= limit; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= limit; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        
+        List<Integer> primes = new ArrayList<>();
+        for (int num = left; num <= right; num++) {
+            if (isPrime[num]) {
+                primes.add(num);
+            }
+        }
+
+    
+        if (primes.size() < 2) {
+            return new int[]{-1, -1}; 
+        }
+
+        int minDiff = Integer.MAX_VALUE;
+        int[] result = new int[2];
+
+        for (int i = 0; i < primes.size() - 1; i++) {
+            int diff = primes.get(i + 1) - primes.get(i);
+            if (diff < minDiff) {
+                minDiff = diff;
+                result[0] = primes.get(i);
+                result[1] = primes.get(i + 1);
+            }
+        }
+
+        return result;
+    }
+
+    
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(Arrays.toString(solution.closestPrimes(10, 19))); 
+        System.out.println(Arrays.toString(solution.closestPrimes(4, 6)));   
+    }
+}
+
