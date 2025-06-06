79b2a30413b72666cfc1186b4df41431aab30acf
Ankit Singh <133861762+singhankit6748@users.noreply.github.com>
2025-06-06 10:04:53 +0530

    Update solution.java

--- maximum-number-of-integers-to-choose-from-a-range-i/solution.java
+++ maximum-number-of-integers-to-choose-from-a-range-i/solution.java
@@ -1,1 +1,23 @@
- 
+ import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    public int maxCount(int[] banned, int n, int maxSum) {
+        Set<Integer> bannedSet = new HashSet<>();
+        for (int num : banned) {
+            bannedSet.add(num);
+        }
+
+        int count = 0, sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (bannedSet.contains(i)) continue;
+            if (sum + i > maxSum) break;
+            sum += i;
+            count++;
+        }
+
+        return count;
+    }
+}
+
