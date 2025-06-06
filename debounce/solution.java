e0b554527f877f50dcc4cd4a967bb750e4aec5dc
Ankit Singh <133861762+singhankit6748@users.noreply.github.com>
2025-06-06 09:52:24 +0530

    Update solution.java

--- debounce/solution.java
+++ debounce/solution.java
@@ -1,1 +1,8 @@
  
+var debounce = function(fn, t = 1000) {
+    let timer;
+    return function(...args) {
+        clearTimeout(timer);
+        timer = setTimeout(() => fn(...args), t);
+    }
+};
