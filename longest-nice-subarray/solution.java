97203c1185503c6a02971361153887a9ff8149cb
Ankit Singh <133861762+singhankit6748@users.noreply.github.com>
2025-06-12 21:32:26 +0530

    Update solution.java

--- longest-nice-subarray/solution.java
+++ longest-nice-subarray/solution.java
@@ -1,1 +1,28 @@
- 
+ public class Solution {
+    public int longestNiceSubarray(int[] nums) {
+        int left = 0, maxLen = 0, bitMask = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            while ((bitMask & nums[right]) != 0) {
+                bitMask ^= nums[left];
+                left++;
+            }
+
+            bitMask |= nums[right];
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        int[] nums1 = {1, 3, 8, 48, 10};
+        System.out.println(sol.longestNiceSubarray(nums1)); 
+
+        int[] nums2 = {3, 1, 5, 11, 13};
+        System.out.println(sol.longestNiceSubarray(nums2)); 
+    }
+}
+
