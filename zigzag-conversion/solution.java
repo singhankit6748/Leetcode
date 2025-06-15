23c3a08b9b182d0c88c6d7f30ce977b92acfb383
Ankit Singh <133861762+singhankit6748@users.noreply.github.com>
2025-06-15 20:29:36 +0530

    Update solution.java

--- zigzag-conversion/solution.java
+++ zigzag-conversion/solution.java
@@ -1,1 +1,29 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        if (numRows == 1 || numRows >= s.length())
+            return s;
+
+        StringBuilder[] rowsValue = new StringBuilder[numRows];
+        for (int i = 0; i < numRows; i++)
+            rowsValue[i] = new StringBuilder();
+
+        int currRow = 0;
+        boolean isGoingDown = false;
+
+        for (char chr : s.toCharArray()) {
+            rowsValue[currRow].append(chr);
+
+            if (currRow == 0 || currRow == numRows - 1)
+                isGoingDown = !isGoingDown;
+
+            currRow += (isGoingDown) ? 1 : -1;
+        }
+
+        StringBuilder res = new StringBuilder();
+        for (StringBuilder row : rowsValue) 
+            res.append(row);
+
+        return res.toString();
+    }
+}
  
