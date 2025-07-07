56ded8df64499763fb3681dc6eed7950438692bc
Ankit Singh <133861762+singhankit6748@users.noreply.github.com>
2025-07-07 17:45:37 +0530

    Time: 6 ms (16.15%) | Memory: 44.4 MB (95.67%) - LeetSync

---
+++ 95-unique-binary-search-trees-ii/unique-binary-search-trees-ii.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left, right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+class Solution {
+    private Map<String, List<TreeNode>> memo = new HashMap<>();
+
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0) return new ArrayList<>();
+        return buildTrees(1, n);
+    }
+
+    private List<TreeNode> buildTrees(int start, int end) {
+        String key = start + "," + end;
+        if (memo.containsKey(key)) {
+            return memo.get(key);
+        }
+
+        List<TreeNode> trees = new ArrayList<>();
+        if (start > end) {
+            trees.add(null);
+            return trees;
+        }
+
+        for (int i = start; i <= end; i++) {
+            List<TreeNode> leftSubtrees = buildTrees(start, i - 1);
+            List<TreeNode> rightSubtrees = buildTrees(i + 1, end);
+
+            for (TreeNode left : leftSubtrees) {
+                for (TreeNode right : rightSubtrees) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = left;
+                    root.right = right;
+                    trees.add(root);
+                }
+            }
+        }
+
+        memo.put(key, trees);
+        return trees;
+    }
+}
