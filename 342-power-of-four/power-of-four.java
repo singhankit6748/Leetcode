657cfea8b4f7e1ff9f02eafc9cc5ae895d4dfc73
Ankit Singh <133861762+singhankit6748@users.noreply.github.com>
2025-08-15 12:48:40 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (8.89%) - LeetSync

--- 342-power-of-four/power-of-four.java
+++ 342-power-of-four/power-of-four.java
@@ -1,8 +1,5 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        if (n <= 0) {
-            return false;
-        }
-        return Math.log(n) / Math.log(4) % 1 == 0;        
+        return n > 0 && (n & (n - 1)) == 0 && (n & 0x55555555) != 0;
     }
 }
